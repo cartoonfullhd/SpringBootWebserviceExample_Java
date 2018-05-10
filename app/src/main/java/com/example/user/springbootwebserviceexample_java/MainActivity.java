1364cc20142a63d2b7900a2c97f9953aeb42d7ec
cartoonfullhd <cartoonfullhd@gmail.com>
2018-05-10 15:33:46 +0700

    Add button
    Add 3 buttons to update, delete, and add book to WebService and create show result of row and response code. Have 3 edit text to input id, name, and author of book.

--- app/src/main/java/com/example/user/springbootwebserviceexample_java/MainActivity.java
+++ app/src/main/java/com/example/user/springbootwebserviceexample_java/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.user.springbootwebserviceexample_java;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -14,20 +18,75 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.charset.Charset;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editIdBook;
+    EditText editNameBook;
+    EditText editAuthorBook;
+    TextView resultResponse;
+    Button addBtn, deleteBtn, updateBtn;
+    String txtName, txtAuthor, txtId;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editIdBook = findViewById(R.id.inputIdBook);
+        editNameBook = findViewById(R.id.inputNameBook);
+        editAuthorBook = findViewById(R.id.inputAuthorBook);
+        resultResponse = findViewById(R.id.resultResponse);
+        addBtn = findViewById(R.id.addButton);
+        deleteBtn = findViewById(R.id.deleteButton);
+        updateBtn = findViewById(R.id.updateButton);
         new getBook().execute();
+
+        addBtn.setOnClickListener(new View.OnClickListener()
+        {
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    txtName = editNameBook.getText().toString();
+                    txtAuthor = editAuthorBook.getText().toString();
+                    if(TextUtils.isEmpty(txtName) || TextUtils.isEmpty(txtAuthor))
+                    {
+                        editNameBook.setError("Input name of book");
+                        editAuthorBook.setError("Input author of book");
+                    }
+                    else{
+                            String[] bookData = {txtName, txtAuthor};
+                            new addBook().execute(bookData);
+                    }
+                }
+        });
+
+        deleteBtn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                // TODO Auto-generated method stub
+                txtName = editNameBook.getText().toString();
+                if(TextUtils.isEmpty(txtName))
+                {
+                    editNameBook.setError("Input name of book");
+                }
+                else{
+                    new deleteBook().execute(txtName);
+                }
+            }
+        });
+
     }
 
     private class getBook extends AsyncTask<Void, Void, String>
     {
+        @Override
+        protected void onPreExecute()
+        {
+
+        }
+
         @Override
         protected String doInBackground(Void ... params)
         {
@@ -37,8 +96,9 @@ public class MainActivity extends AppCompatActivity {
             String JSONResult = "";
             try
             {
-                url = new URL("http://192.168.3.108:8080/books");
+                url = new URL(getResources().getString(R.string.ip_address));
                 urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
                 //InputStream decode urlConnection to Byte format
                 InputStream Byte = urlConnection.getInputStream();
                 //InputStreamReader decode Byte format to Char format
@@ -159,9 +219,76 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
+        protected void onProgressUpdate(Void... values)
+        {
+
+        }
+    }
+
+    private class addBook extends AsyncTask<String[], Void, String>
+    {
+        public int code;
+        @Override
         protected void onPreExecute()
         {
 
+        }
+
+        @Override
+        protected String doInBackground(String[]... params)
+        {
+            URL url;
+            HttpURLConnection urlConnection = null;
+            String line;
+            String JSONResult = "";
+            String[] parameter = params[0];
+            String urlParameters  = "name=" + parameter[0] + "&author=" + parameter[1];
+            byte[] postData       = urlParameters.getBytes(Charset.forName("UTF-8"));
+            int    postDataLength = postData.length;
+            try
+            {
+                url = new URL(getResources().getString(R.string.ip_address_add));
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+                urlConnection.getOutputStream().write(postData);
+                code = urlConnection.getResponseCode();
+
+                //InputStream decode urlConnection to Byte format
+                InputStream Byte = urlConnection.getInputStream();
+                //InputStreamReader decode Byte format to Char format
+                InputStreamReader Char = new InputStreamReader(Byte);
+                BufferedReader reader = new BufferedReader(Char);
+                StringBuilder builder = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line + "\n");
+                }
+
+                reader.close();
+                JSONResult = builder.toString();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if(urlConnection != null)
+                {
+                    urlConnection.disconnect();
+                }
+            }
+            return JSONResult;
+        }
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            super.onPostExecute(result);
+            resultResponse.setText("Number of row was updated: " + result + "\n" + "Response code:  " + code);
         }
 
         @Override
@@ -171,4 +298,76 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private class deleteBook extends AsyncTask<String, Void, String>
+    {
+        public int code;
+        @Override
+        protected void onPreExecute()
+        {
+
+        }
+
+        @Override
+        protected String doInBackground(String... params)
+        {
+            URL url;
+            HttpURLConnection urlConnection = null;
+            String line;
+            String name = params[0];
+            String JSONResult = "";
+            String urlParameters  = "name=" + name;
+            //int    postDataLength = postData.length;
+            try
+            {
+                url = new URL(getResources().getString(R.string.ip_address_delete)+"?"+urlParameters);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("DELETE");
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                urlConnection.setRequestProperty("charset", "utf-8");
+
+                code = urlConnection.getResponseCode();
+
+                //InputStream decode urlConnection to Byte format
+                InputStream Byte = urlConnection.getInputStream();
+                //InputStreamReader decode Byte format to Char format
+                InputStreamReader Char = new InputStreamReader(Byte);
+                BufferedReader reader = new BufferedReader(Char);
+                StringBuilder builder = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line + "\n");
+                }
+
+                reader.close();
+                JSONResult = builder.toString();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if(urlConnection != null)
+                {
+                    urlConnection.disconnect();
+                }
+            }
+            return JSONResult;
+        }
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            super.onPostExecute(result);
+            resultResponse.setText("Number of row was updated: " + result + "\n" + "Response code:  " + code);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values)
+        {
+
+        }
+    }
 }
